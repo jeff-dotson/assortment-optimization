e645c25e43391b178f20998962ddf97ebd0ffb18
Garrett Carr <fyrefoxgc@gmail.com>
2021-03-01 13:26:21 -0700

    Added functions for calling the API. It was an easier solution than I thought to add one column for the image srcs.

---
+++ Code/02 - API Functions.R
@@ -0,0 +1,33 @@
+# This is the file used to get image URL's associated with the barcodes
+# NOTE: you will need an API key.
+
+library(httr)
+library(jsonlite)
+library(tidyverse)
+
+barcode_lookup <- "https://api.barcodelookup.com/v2/products?"
+
+# Function used to grab the image URL's using barcodelookup.com
+# Make sure to provide the barcode
+barcode_image_lookup <- function(barcode) {
+  img_src <-
+    GET(url = paste(barcode_lookup, "barcode=", barcode, "&key=", key, sep = ""))
+  if (img_src$status_code == 200) { 
+## Only compute if the product is found in barcodelookup database.
+    img <- content(img_src, "parsed") %>%
+      flatten()
+    unlist(img$products$images)
+  }
+  else {
+    img <- NA
+    img
+  }
+}
+
+# Function used to add additional columns to a look up table
+
+add_image_src <- function(tbl, barcode) {
+  tbl %>%
+    rowwise() %>%
+    mutate(img_src = barcode_image_lookup({{ barcode }}))
+}
